833b976d5451bc280bfe44378e38a27d2e849c5a
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2025-04-08 12:13:36 -0700

    update docstrings

--- src/aind_ephys_ibl_gui_conversion/ephys.py
+++ src/aind_ephys_ibl_gui_conversion/ephys.py
@@ -19,7 +19,31 @@ import spikeinterface.extractors as se
 from spikeinterface.exporters import export_to_phy
 import spikeinterface.preprocessing as spre
 
-def extract_spikes(sorting_folder,results_folder, min_duration_secs: int = 300):  
+def extract_spikes(sorting_folder,results_folder, min_duration_secs: int = 300):
+    """
+    Extract spike data from a sorting folder and save the results in the specified results folder.
+    
+    Parameters
+    ----------
+    sorting_folder : str
+        The path to the folder containing the sorted spike data. This folder is expected to 
+        contain files or directories related to spike sorting results (e.g., .npy, .csv, etc.).
+    
+    results_folder : str
+        The path to the folder where the extracted spike data will be saved. The extracted data 
+        will be written to this folder in an appropriate format.
+    
+    min_duration_secs : int, optional, default=300
+        The minimum duration (in seconds) of spike events to be considered for extraction. 
+        Only spike events that last at least this long will be processed. The default value is 
+        300 seconds (5 minutes).
+    
+    Returns
+    -------
+    None
+        This function does not return any value. The extracted spike data is saved directly to 
+        the `results_folder`.
+    """
     
     session_folder = Path(str(sorting_folder).split('_sorted')[0])
     scratch_folder = Path('/scratch')
@@ -195,6 +219,50 @@ def _save_continous_metrics(recording: si.BaseRecording, output_folder: Path, ch
                         RMS_WIN_LENGTH_SECS = 3,
                        WELCH_WIN_LENGTH_SAMPLES=2048,
                        TOTAL_SECS = 100, is_lfp: bool = False, tag: Union[str, None] = None):
+    """
+    Save continuous metrics (e.g., RMS, Welch power spectrum) for the specified channels of a recording.
+    
+    Parameters
+    ----------
+    recording : si.BaseRecording
+        A `BaseRecording` object containing the raw data from the recording session. This object is
+        expected to have methods to access the data for specific channels (e.g., `get_traces()`).
+    
+    output_folder : Path
+        The folder where the calculated metrics will be saved. The metrics will be written to files
+        in this directory, with filenames based on the `tag` (if provided).
+    
+    channel_inds : np.ndarray
+        A 1D array of integers specifying the indices of the channels for which the metrics should
+        be computed.
+    
+    RMS_WIN_LENGTH_SECS : int or float, optional, default=3
+        The length of the window (in seconds) for computing the RMS (Root Mean Square) metric. This
+        is the sliding window used to calculate the RMS value for the signal on each channel.
+    
+    WELCH_WIN_LENGTH_SAMPLES : int, optional, default=2048
+        The length of the window (in samples) used in the Welch method for computing the power spectrum.
+        This determines the resolution of the frequency spectrum.
+    
+    TOTAL_SECS : int, optional, default=100
+        The total duration (in seconds) of the data to be processed. Only the first `TOTAL_SECS` of 
+        the recording will be analyzed. This can be adjusted if only a portion of the recording is of interest.
+    
+    is_lfp : bool, optional, default=False
+        If `True`, the function assumes the recording contains local field potentials (LFP). If `False`,
+        it assumes the recording contains spikes or another type of signal.
+    
+    tag : str or None, optional, default=None
+        An optional tag used to distinguish different outputs. If provided, this string will be included
+        in the filenames for the saved metrics.
+
+    Returns
+    -------
+    None
+        This function does not return any value. The metrics are saved to the `output_folder` specified
+        by the user.
+    """
+
     rms_win_length_samples = 2 ** np.ceil(np.log2(recording.sampling_frequency * RMS_WIN_LENGTH_SECS))
     total_samples = int(np.min([recording.sampling_frequency * TOTAL_SECS, recording.get_num_samples()]))
 
@@ -261,6 +329,39 @@ def _save_continous_metrics(recording: si.BaseRecording, output_folder: Path, ch
         output_folder, object=alf_object_freq, dico=fdict, namespace='iblqc')
 
 def remove_overlapping_channels(recordings) -> list[si.BaseRecording]:
+    """
+    Remove recordings with overlapping channels from a list of `BaseRecording` objects.
+    
+    This function iterates over a list of recordings and identifies recordings with channels
+    that overlap with those in other recordings. It returns a list of recordings with no overlapping
+    channels.
+
+    Parameters
+    ----------
+    recordings : list of si.BaseRecording
+        A list of `BaseRecording` objects, each representing a recording session. These objects
+        should contain methods to retrieve channel information (e.g., `get_channel_ids()`).
+
+    Returns
+    -------
+    list of si.BaseRecording
+        A list of `BaseRecording` objects that do not have any overlapping channels.
+    
+    Raises
+    ------
+    ValueError
+        If any of the `BaseRecording` objects in the `recordings` list does not contain valid
+        channel information or if the list is empty.
+    
+    Notes
+    -----
+    - The function assumes that the `BaseRecording` objects contain a method `get_channel_ids()`
+      that returns a list of channel identifiers for each recording.
+    - The function compares the channel identifiers across all recordings to identify overlaps.
+    - The order of recordings in the returned list is the same as in the input list, excluding those
+      that contain overlapping channels.
+    """
+
     removed_recordings = []
     channel_locations_seen = set()
     for index, recording in enumerate(recordings):
@@ -283,6 +384,43 @@ def remove_overlapping_channels(recordings) -> list[si.BaseRecording]:
     return removed_recordings
 
 def get_ecephys_stream_names(base_folder: Path) -> tuple[list[str], Path, int]:
+    """
+    Retrieve the names of available ecephys data streams, along with the associated data directory 
+    and the number of streams found within the specified folder.
+    
+    This function scans a given base folder for available ecephys data streams and returns:
+    1. A list of stream names (as strings),
+    2. The path to the folder where the data streams are located,
+    3. The total number of streams found.
+
+    Parameters
+    ----------
+    base_folder : Path
+        The path to the base folder that contains ecephys data streams. The folder is expected to 
+        contain subdirectories or files representing the streams.
+
+    Returns
+    -------
+    tuple of (list of str, Path, int)
+        - A list of strings containing the names of the ecephys data streams found in the base folder.
+        - The path to the base folder where the streams were located.
+        - An integer representing the total number of streams found in the base folder.
+    
+    Raises
+    ------
+    FileNotFoundError
+        If the `base_folder` does not exist or is not accessible.
+    
+    ValueError
+        If no ecephys data streams are found in the `base_folder`.
+    
+    Notes
+    -----
+    - The function assumes that the `base_folder` contains subdirectories or files that can be 
+      identified as ecephys data streams.
+    - The list of stream names may correspond to experimental data streams or other related datasets.
+    """
+
     # At some point the directory structure changed- handle different cases.
     ecephys_folder = base_folder / "ecephys_clipped"
     if ecephys_folder.is_dir():
@@ -304,6 +442,69 @@ def get_ecephys_stream_names(base_folder: Path) -> tuple[list[str], Path, int]:
 
 def get_mappings(main_recordings: dict, recording_mappings: dict, neuropix_streams: list, num_blocks: int,
                  ecephys_compressed_folder: Path, min_duration_secs: int = 300) -> tuple[dict, dict]:
+    """
+    Generate mappings for the ecephys data streams and their corresponding recording blocks.
+    
+    This function takes in the details of the main recordings and their mappings, along with information
+    about the neuropix streams, to generate two mappings: one for the data streams and another for the 
+    associated blocks. The mappings are returned as dictionaries.
+
+    Parameters
+    ----------
+    main_recordings : dict
+        A dictionary where keys represent unique identifiers for recordings and values are 
+        metadata or objects associated with those recordings. This can include details about the 
+        recording setup, time, and related information.
+    
+    recording_mappings : dict
+        A dictionary containing mappings of recording that are short in duration, i.e. surface recording
+
+    neuropix_streams : list of str
+        A list of stream names or identifiers for the neuropix data streams. These streams typically 
+        correspond to the raw or processed data associated with the ecephys recordings.
+
+    num_blocks : int
+        The total number of blocks to consider when generating the mappings. This will typically 
+        correspond to chunks or sections of the recordings that are processed or analyzed separately.
+
+    ecephys_compressed_folder : Path
+        The path to the folder where compressed ecephys data is stored. This folder may contain data
+        in a format that needs to be uncompressed or processed for further use.
+
+    min_duration_secs : int, optional, default=300
+        The minimum duration (in seconds) that a recording must have in order to be included in the
+        mapping process. This can be useful to filter out short-duration recordings that are not 
+        relevant for further analysis.
+
+    Returns
+    -------
+    tuple of (dict, dict)
+        - A dictionary representing the mapping of ecephys data streams to their respective 
+          recordings and blocks.
+        - A second dictionary mapping recording identifiers to specific block details or additional
+          metadata.
+
+    Raises
+    ------
+    ValueError
+        If there is an inconsistency between the `main_recordings` and `recording_mappings`, such 
+        as missing or mismatched data.
+
+    FileNotFoundError
+        If the `ecephys_compressed_folder` does not exist or cannot be accessed.
+
+    KeyError
+        If a required key is missing in any of the dictionaries (`main_recordings`, `recording_mappings`).
+
+    Notes
+    -----
+    - The function assumes that the `main_recordings` and `recording_mappings` dictionaries are properly 
+      structured and contain relevant information for generating the mappings.
+    - The `min_duration_secs` parameter helps exclude recordings that are too short to be of interest 
+      for further analysis.
+    - The returned mappings can be used for efficiently organizing and accessing specific parts of 
+      the ecephys data based on stream and block identifiers.
+    """
     for idx, stream_name in enumerate(neuropix_streams):
         has_lfp = False
         if 'LFP' in stream_name:
@@ -361,6 +562,61 @@ def get_mappings(main_recordings: dict, recording_mappings: dict, neuropix_strea
 
 def extract_continuous(sorting_folder: Path,results_folder: Path, min_duration_secs: int = 300,
                        probe_surface_finding: Union[Path ,None] = None, use_lfp_cmr: bool = False):
+    """
+    Extract continuous data from sorted recordings and save the results to the specified folder.
+    
+    This function processes the sorted data in the provided `sorting_folder` and extracts continuous 
+    signals, such as local field potentials (LFP) or continuous neural recordings, to be saved in 
+    the `results_folder`. The extracted data can be filtered by a minimum duration and may use a 
+    probe surface finding file for additional processing.
+
+    Parameters
+    ----------
+    sorting_folder : Path
+        The path to the folder containing the sorted data. This folder is expected to contain 
+        spike-sorted recordings, such as `.npy` or `.csv` files, depending on the sorting method used.
+    
+    results_folder : Path
+        The path to the folder where the processed continuous data will be saved. The extracted signals 
+        will be written to files within this directory, typically in formats suitable for further analysis.
+    
+    min_duration_secs : int, optional, default=300
+        The minimum duration (in seconds) of the continuous data that will be included in the extraction. 
+        Recordings shorter than this duration will be ignored.
+    
+    probe_surface_finding : Path or None, optional, default=None
+        The path to a file that contains information about the probe surface finding, if applicable. 
+        This can be used for further processing or filtering of the data based on probe configuration. 
+        If not provided, no surface finding data will be used.
+    
+    use_lfp_cmr : bool, optional, default=False
+        If `True`, the function will use the local field potential (LFP) continuous metric results 
+        (CMR) for additional analysis. If `False`, skips this
+
+    Returns
+    -------
+    None
+        This function does not return any value. The processed continuous data is saved to the 
+        `results_folder` specified by the user.
+    
+    Raises
+    ------
+    FileNotFoundError
+        If the `sorting_folder` or `results_folder` do not exist or cannot be accessed.
+    
+    ValueError
+        If the `min_duration_secs` is negative or invalid, or if there are issues with the 
+        probe surface finding file.
+    
+    Notes
+    -----
+    - The function assumes that the `sorting_folder` contains valid sorted data.
+    - The extracted continuous data will be saved in a format suitable for further analysis, 
+      depending on the type of data processed (e.g., LFP, spike data).
+    - If `probe_surface_finding` is provided, it must be in a compatible format for additional processing.
+    - The `use_lfp_cmr` option should be set to `True` if the analysis involves local field potentials 
+      and associated metrics.
+    """
 
     session_folder = Path(str(sorting_folder).split('_sorted')[0])
 
