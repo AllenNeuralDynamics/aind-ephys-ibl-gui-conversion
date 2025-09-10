e59dbc29b1eb375a9a26eb118a6529a1f62179ae
yonibrowning <yonib@uw.edu>
2025-09-09 19:10:50 -0700

    Update ephys.py
    Switched reader to better parse quad base

--- src/aind_ephys_ibl_gui_conversion/ephys.py
+++ src/aind_ephys_ibl_gui_conversion/ephys.py
@@ -92,7 +92,7 @@ def extract_spikes(  # noqa: C901
     )
 
     neuropix_streams = [s for s in stream_names if "Neuropix" in s]
-    probe_names = [s.split(".")[1].split("-")[0] for s in neuropix_streams]
+    probe_names = [s.split(".")[1].split("_")[0] for s in neuropix_streams]
 
     for idx, stream_name in enumerate(neuropix_streams):
         analyzer_mappings = []
