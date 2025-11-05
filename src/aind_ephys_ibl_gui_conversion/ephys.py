3d139c35638f32c68ac425c536ead6e0eb8bcb5d
yonibrowning <yonib@uw.edu>
2025-11-05 15:24:07 -0800

    Update ephys.py
    Handles AP band properly

--- src/aind_ephys_ibl_gui_conversion/ephys.py
+++ src/aind_ephys_ibl_gui_conversion/ephys.py
@@ -99,7 +99,7 @@ def extract_spikes(  # noqa: C901
     )
 
     neuropix_streams = [s for s in stream_names if "Neuropix" in s]
-    probe_names = [s.split(".")[1].split("_")[0] for s in neuropix_streams]
+    probe_names = [s.split(".")[1].split("_")[0].split("-AP")[0] for s in neuropix_streams]
 
     print("Probe names",probe_names)
 
@@ -1029,7 +1029,7 @@ def extract_continuous(  # noqa: C901
 
         print(stream_name)
 
-        probe_name = stream_name.split(".")[1].split("_")[0]
+        probe_name = stream_name.split(".")[1].split("_")[0].split("-AP")[0]
         print(probe_name)
 
         output_folder = Path(results_folder) / probe_name
